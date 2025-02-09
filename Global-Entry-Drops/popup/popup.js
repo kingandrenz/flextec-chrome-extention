85c3606f987573a186153c597eb00fe38f6574c5
flexteck <flexteckse@gmail.com>
2025-02-09 15:26:43 +0100

    validating input and showing error messages

--- Global-Entry-Drops/popup/popup.js
+++ Global-Entry-Drops/popup/popup.js
@@ -7,22 +7,103 @@ const endDateElement = document.getElementById("enddate");
 const startButton = document.getElementById("startbutton");
 const stopButton = document.getElementById("stopbutton");
 
+// span listenner
+const runningSpan = document.getElementById("runningSpan");
+const stoppedSpan = document.getElementById("stoppedSpan");
+
+// Error Message
+const locationIdError = document.getElementById("locationIdError");
+const startDateError = document.getElementById("startDateError");
+const endDateError = document.getElementById("endDateError");
+
+const hideElement = (elem) => {
+  elem.style.display = "none";
+};
+
+const showElement = (elem) => {
+  elem.style.display = "";
+};
+
+const enableElement = (elem) => {
+  elem.disabled = false;
+};
+
+const disableElement = (elem) => {
+  elem.disabled = true;
+};
+
+const handleOnStartState = () => {
+  // spans
+  showElement(runningSpan);
+  hideElement(stoppedSpan);
+  // buttons
+  disableElement(startButton);
+  enableElement(stopButton);
+  //disable Element
+  disableElement(locationIdElement);
+  disableElement(startDateElement);
+  disableElement(endDateElement);
+};
+
+const handleOnStopState = () => {
+  // spans
+  showElement(stoppedSpan);
+  hideElement(runningSpan);
+  // button
+  disableElement(stopButton);
+  enableElement(startButton);
+  // enable element
+  enableElement(locationIdElement);
+  enableElement(startDateElement);
+  enableElement(endDateElement);
+};
+
+const performOnStartValidation = () => {
+  if (!locationIdElement.value) {
+    showElement(locationIdError);
+  } else {
+    hideElement(locationIdError);
+  }
+
+  if (!startDateElement.value) {
+    showElement(startDateError);
+  } else {
+    hideElement(startDateError);
+  }
+
+  if (!endDateElement.value) {
+    showElement(endDateError);
+  } else {
+    hideElement(endDateError);
+  }
+
+  return (
+    locationIdElement.value && startDateElement.value && endDateElement.value
+  );
+};
+
 // listening to events
 
 startButton.onclick = function () {
-  const prefs = {
-    locationId: locationIdElement.value,
-    startDate: startDateElement.value,
-    endDate: endDateElement.value,
-    tzData:
-      locationIdElement.options[locationIdElement.selectedIndex].getAttribute(
-        "data-tz"
-      ),
-  };
-  chrome.runtime.sendMessage({ event: "onStart", prefs }); // send message to the background.js
+  const allFieldsValid = performOnStartValidation();
+
+  if (allFieldsValid) {
+    handleOnStartState();
+    const prefs = {
+      locationId: locationIdElement.value,
+      startDate: startDateElement.value,
+      endDate: endDateElement.value,
+      tzData:
+        locationIdElement.options[locationIdElement.selectedIndex].getAttribute(
+          "data-tz"
+        ),
+    };
+    chrome.runtime.sendMessage({ event: "onStart", prefs }); // send message to the background.js
+  }
 };
 
 stopButton.onclick = function () {
+  handleOnStopState();
   chrome.runtime.sendMessage({ event: "onStop" });
 };
 
@@ -41,7 +122,12 @@ chrome.storage.local.get(
 
     if (endDate) endDateElement.value = endDate;
 
-    console.log("Runnig Status:", isRunning);
+    if (isRunning) {
+      console.log("running:", runningSpan, isRunning);
+      handleOnStartState();
+    } else {
+      handleOnStopState();
+    }
   }
 );
 
